7fb79cee25dc1d1af1bbf1afb0d7f2c57c2b3598
trcrsired <uwgghhbcad@gmail.com>
2023-05-01 20:13:29 -0400

    mm

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,23 @@
+#include"imports/impl.h"
+
+void* __stdcall wine_unix_call_model( ::std::uint_least64_t handle, ::std::uint_least32_t code, void *args ) noexcept;
+
 int main()
 {
 	auto mdptr{::winelibc::GetModuleHandleW(u"ntdll.dll")};
-	::winelibc::GetProcAddress(mdptr);
+	if(mdptr==nullptr)
+	{
+		__builtin_trap();
+	}
+	auto wineunixcall = ::winelibc::GetProcAddress(mdptr,reinterpret_cast<char const*>(u8"__wine_unix_call"));
+
+	if(wineunixcall==nullptr)
+	{
+		__builtin_trap();
+	}
+
+	auto __wine_unix_call{reinterpret_cast<decltype(wine_unix_call_model)*>(wineunixcall)};
+
+	__wine_unix_call();
+//	__builtin_printf("unxc:%p\n",unxc);
 }
